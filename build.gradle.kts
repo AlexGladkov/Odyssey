a7325d76ecb4da8fe2c95874efb62db2bad294b4
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2021-12-04 15:24:07 +0200

    add library for tracker time build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
-import org.gradle.internal.impldep.org.codehaus.plexus.util.Os
-
 plugins {
     `kotlin-dsl`
+    id("net.rdrei.android.buildtimetracker") version "0.11.0"
 }
+
+apply(plugin = "net.rdrei.android.buildtimetracker")
 
 val compileSdkVersion by extra(31)
 val targetSdkVersion by extra(30)
@@ -15,6 +16,7 @@ allprojects {
         mavenLocal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://jitpack.io")
+        maven("https://plugins.gradle.org/m2/")
     }
 }
 
@@ -22,5 +24,16 @@ buildscript {
     val kotlin_version by extra("1.5.31")
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("net.rdrei.android.buildtimetracker:gradle-plugin:0.11.0")
+    }
+}
+
+buildtimetracker {
+    reporters {
+        register("summary") {
+            options["ordered"] = "true"
+            options["barstyle"] = "none"
+            options["shortenTaskNames"] = "false"
+        }
     }
 }
