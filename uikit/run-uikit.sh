ddf81674165dcdcfd5895de5ba2bb6d75a0a21b0
dima.avdeev <dima.avdeev@jetbrains.com>
2022-06-10 21:26:49 +0400

    remove redundant

--- uikit/run-uikit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-./gradlew iosDeployIPhone13Debug
--- uikit/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-includeBuild("../") {
-    dependencySubstitution {
-        substitute(module("ru.alexgladkov:comon-common-root")).using(project(":common:common-root"))
-        substitute(module("ru.alexgladkov:odyssey-odyssey-core")).using(project(":odyssey:odyssey-core"))
-        substitute(module("ru.alexgladkov:odyssey-odyssey-compose")).using(project(":odyssey:odyssey-compose"))
-        substitute(module("ru.alexgladkov:common-common-root")).using(project(":common:common-root"))
-    }
-}
-
